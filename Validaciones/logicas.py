aea5db0848d4b5e7c59a98ffd082677c34db883b
Andres <eiderpira880@gmail.com>
2025-10-29 07:30:33 -0500

    Se hicieron cambios

--- Validaciones/logicas.py
+++
@@ -1,3 +0,0 @@
-# Evitar duplicados (por ejemplo, ID o nombre ya existente)
-# Reglas específicas del sistema (por ejemplo, stock no negativo)
-# Relación entre datos (por ejemplo, fecha de inicio antes que fecha final)
