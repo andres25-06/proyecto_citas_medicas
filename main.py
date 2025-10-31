5f8b3995b3a76e86c0ce2bcaaf11e95a7cbb00c5
Jimy Sebastian Angarita Triana <jimy_sangarita@soy.sena.edu.co>
2025-10-30 22:59:32 -0500

    Se modifico el archivo main

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # main.py
-
+from Vista.vista_login import login
 from Vista.vista_principal import vista_principal
 
+def main():
+    usuario = login()
+    if usuario:
+        vista_principal()
+
 if __name__ == "__main__":
-    vista_principal()
+    main()
+
